ae47e01f4b667863f912f80482016bb93b1e70ec
Sebastian Camargo <juansecapri@hotmail.com>
2024-04-16 10:26:43 -0500

    Avances sección 3

--- bases/tipos/strings.js
+++ bases/tipos/strings.js
@@ -1,5 +1,11 @@
 "use strict";
 (() => {
+    var _a;
     const batman = 'Batman';
-    const linternaVerde = 'Linterna Verde';
+    const linternaVerde = "Green Lantern";
+    const superMan = `Héroe: Superman`;
+    console.log(`I'm ${linternaVerde}`);
+    console.log(batman.toUpperCase());
+    console.log(((_a = batman[10]) === null || _a === void 0 ? void 0 : _a.toUpperCase()) || 'No tiene un valor');
+    console.log(superMan);
 })();
--- bases/tipos/strings.ts
+++ bases/tipos/strings.ts
@@ -1,7 +1,15 @@
 (() => {
 
-    const batman = 'Batman';
-    const linternaVerde = 'Linterna Verde';
+    const batman: string = 'Batman';
+    const linternaVerde: string = "Green Lantern";
+    const superMan: string = `Héroe: Superman`;
 
+    console.log(`I'm ${linternaVerde}`);
+
+    console.log(batman.toUpperCase());
+
+    console.log(batman[10]?.toUpperCase() || 'No tiene un valor');
+
+    console.log(superMan);
 
 })();
