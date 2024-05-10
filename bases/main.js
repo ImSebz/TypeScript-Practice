0267dc22e9cf5de96363addec522ee336574e9da
Sebastian Camargo <juansecapri@hotmail.com>
2024-05-10 10:18:00 -0500

    Avances sección 10

--- bases/main.js
+++ bases/main.js
@@ -1,127 +1,13 @@
 "use strict";
-(() => {
-    const sumar = (a, b) => {
-        return a + b;
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length > 0) ? true : false;
     };
-    const contar = (heroes) => {
-        return heroes.length;
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf())) ? false : true;
     };
-    const superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
-    contar(superHeroes);
-    const llamarBatman = (llamar = true) => {
-        if (llamar) {
-        }
-    };
-    const unirheroes = (...personas) => {
-        return personas.join(", ");
-    };
-    const noHaceNada = (numero, texto, booleano, arreglo) => { };
-    let noHaceNadaTampoco;
-    noHaceNadaTampoco = noHaceNada;
-})();
-(() => {
-    const batimovil = {
-        carroceria: "Negra",
-        modelo: "6x6",
-        antibalas: true,
-        pasajeros: 4
-    };
-    const bumblebee = {
-        carroceria: "Amarillo con negro",
-        modelo: "4x2",
-        antibalas: true,
-        pasajeros: 4,
-        disparar() {
-            console.log("Disparando");
-        }
-    };
-    const villanos = [{
-            nombre: "Lex Luthor",
-            edad: 54,
-            mutante: false
-        }, {
-            nombre: "Erik Magnus Lehnsherr",
-            edad: 49,
-            mutante: true
-        }, {
-            nombre: "James Logan",
-            edad: undefined,
-            mutante: true
-        }];
-    const charles = {
-        poder: "psiquico",
-        estatura: 1.78
-    };
-    const apocalipsis = {
-        lider: true,
-        miembros: ["Magneto", "Tormenta", "Psylocke", "Angel"]
-    };
-    let mystique;
-    mystique = charles;
-    mystique = apocalipsis;
-})();
-(() => {
-    const conducirBatimovil = (auto) => {
-        auto.encender = true;
-        auto.velocidadMaxima = 100;
-        auto.acelear();
-    };
-    const batimovil = {
-        encender: false,
-        velocidadMaxima: 0,
-        acelear() {
-            console.log("...... gogogo!!!");
-        }
-    };
-    const guason = {
-        reir: true,
-        comer: true,
-        llorar: false
-    };
-    const reir = (guason) => {
-        if (guason.reir) {
-            console.log("JAJAJAJA");
-        }
-    };
-    const ciudadGotica = (ciudadanos) => {
-        return ciudadanos.length;
-    };
-    class Persona {
-        imprimirBio() {
-            console.log(`Nombre: ${this.nombre}, Edad: ${this.edad}, Sexo: ${this.sexo}, Estado Civil: ${this.estadoCivil}`);
-        }
-    }
-    const persona = new Persona();
-    persona.nombre = "Bruce Wayne";
-    persona.edad = 35;
-    persona.sexo = "Masculino";
-    persona.estadoCivil = "Soltero";
-})();
-(() => {
-    const batman = 'Bruce';
-    const superman = 'Clark';
-    const existe = false;
-    const parejaHeroes = [batman, superman];
-    const villano = ['Lex Lutor', 5, true];
-    const aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
-    let Fuerza;
-    (function (Fuerza) {
-        Fuerza[Fuerza["acuaman"] = 0] = "acuaman";
-        Fuerza[Fuerza["batman"] = 1] = "batman";
-        Fuerza[Fuerza["flash"] = 5] = "flash";
-        Fuerza[Fuerza["superman"] = 100] = "superman";
-    })(Fuerza || (Fuerza = {}));
-    const fuerzaAcuaman = Fuerza.acuaman;
-    const fuerzaBatman = Fuerza.batman;
-    const fuerzaFlash = Fuerza.flash;
-    const fuerzaSuperman = Fuerza.superman;
-    function activar_batiseñal() {
-        return 'activada';
-    }
-    function pedir_ayuda() {
-        console.log('Auxilio!!!');
-    }
-    const poder = '100';
-    const largoDelPoder = poder.length;
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText("Hello"));
+console.log(Validations.validateDate(new Date()));
 //# sourceMappingURL=main.js.map
